7b045cc04415c8f70e5c9986dda81824afa12a67
ToniIvankovic <toni.ivankovic@gmail.com>
2022-08-18 19:36:45 +0200

    Design improvement

--- src/app/components/game/game.module.ts
+++ src/app/components/game/game.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { GameComponent } from './game.component';
 import { CardModule } from '../card/card.module';
 import { LoaderModule } from '../loader/loader.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [GameComponent],
     exports: [GameComponent],
-    imports: [CommonModule, CardModule, LoaderModule],
+    imports: [CommonModule, CardModule, LoaderModule, MatButtonModule],
 })
 export class GameModule {}
