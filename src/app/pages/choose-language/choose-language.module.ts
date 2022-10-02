467bcf6eb4156fb9810817f8c33a183cdd5cc487
ToniIvankovic <toni.ivankovic@gmail.com>
2022-10-02 16:06:16 +0200

    Added flags to languages

--- src/app/pages/choose-language/choose-language.module.ts
+++ src/app/pages/choose-language/choose-language.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ChooseLanguageComponent } from './choose-language.component';
 import { LoaderModule } from 'src/app/components/loader/loader.module';
 import { MatButtonModule } from '@angular/material/button';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
     declarations: [ChooseLanguageComponent],
     exports: [ChooseLanguageComponent],
-    imports: [CommonModule, LoaderModule, MatButtonModule],
+    imports: [CommonModule, LoaderModule, MatButtonModule, AngularSvgIconModule],
 })
 export class ChooseLanguageModule {}
--- src/app/services/language-data.service.ts
+++ src/app/services/language-data.service.ts
@@ -1,11 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {
-    EMPTY,
-    filter,
     map,
     Observable,
-    of,
     ReplaySubject,
     switchMap,
     tap,
