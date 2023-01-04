ef17bff6ba3379527b38fc56512e5ebee875b3c3
ToniIvankovic <toni.ivankovic@gmail.com>
2023-01-04 12:26:36 +0100

    Transfering key to backend

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 "use strict";
 const express = require("express");
-var {createProxyMiddleware} = require('http-proxy-middleware');
+var { createProxyMiddleware } = require('http-proxy-middleware');
 
-const _port =  process.env.PORT ||  80;
+const _port = process.env.PORT || 80;
 const _app_folder = '.';
 
 const app = express();
@@ -14,14 +14,18 @@ app.use(
     "secure": true,
     "changeOrigin": true,
     "loglevel": "debug",
-    "pathRewrite": {"^/api" : ""},
+    "pathRewrite": { "^/api": "" },
     "cookieDomainRewrite": "localhost"
   })
 );
 
+const key = process.env.private_key;
 const path = "/dist/duolingo-flashcards";
 // ---- SERVE STATIC FILES ---- //
 app.get('*.*', express.static(_app_folder + path, { maxAge: '1y' }));
+app.get("key", (req, res) => {
+  res.send({ key: key });
+});
 
 // ---- SERVE APLICATION PATHS ---- //
 app.all('*', function (req, res) {
--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,20 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable, switchMap, tap } from 'rxjs';
 import { GoogleObj } from '../interfaces/google_obj.interface';
-import { environment } from '../../environments/environment';
+import { TranslationResponse } from '../interfaces/google_obj.interface';
 
 @Injectable({
     providedIn: 'root',
 })
 export class TranslationService {
     url = 'https://translation.googleapis.com/language/translate/v2?key=';
-    // key = environment.private_key;
-    key = process.env['private_key'];
+    private key: Observable<string> = this.http
+        .get('key.json')
+        .pipe(map((res: any) => res.key));
+
     constructor(private http: HttpClient) {}
-    
+
     translate(obj: GoogleObj) {
-        return this.http.post(this.url + this.key, obj, {
-            // headers: { Authorization: `Bearer  ${this.key}` },
-        });
+        return this.key.pipe(
+            switchMap((key) => {
+                return this.http.post<TranslationResponse>(
+                    this.url + this.key,
+                    obj
+                );
+            })
+        );
     }
 }
